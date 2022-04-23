dee3c71f20f5c03e706ef177c509a9b3be9123f6
Guilherme Moraes <102086160+GuilhermeJuli@users.noreply.github.com>
2022-04-23 02:02:30 -0300

    BufferefWriter

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,27 +1,31 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Program {
 
     public static void main(String[] args) {
 
-        String path = "c:\\temp\\in.txt";
+        String[] lines = new String[]{"Good morning","Good afternoon","Good night"};
 
-        try (BufferedReader br = new BufferedReader(new FileReader(path))){
+        String path = "c:\\temp\\out.txt";
 
-            String line = br.readLine();
-
-            while (line != null){
-                System.out.println(line);
-                line = br.readLine();
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(path,true))){
+            for (String line : lines){
+                bw.write(line);
+                bw.newLine();
             }
         }
         catch (IOException e){
-            System.out.println("ERROR" + e.getMessage());
+            e.printStackTrace();
+
         }
+
+
+
 
         }
     }
